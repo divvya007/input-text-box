413ae2e2d8c5ae2596d08639819dd3ae064378b9
Divya <divvyatripaathi@gmail.com>
2021-11-08 14:30:10 +0000

    added input box and css

---
+++ src/component/InputBox/index.js
@@ -0,0 +1,17 @@
+import { useState } from "react";
+import "./style.css";
+
+function InputBox() {
+  return (
+    <>
+      <div className="container">
+        <label>
+          write something:
+          <input />
+        </label>
+      </div>
+    </>
+  );
+}
+
+export default InputBox;
