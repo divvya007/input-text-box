2bc994d188caa48d785d503ebdfcf358e8c1e762
Divya <divvyatripaathi@gmail.com>
2021-11-08 15:11:36 +0000

    added handle on input text box

--- src/component/InputBox/index.js
+++ src/component/InputBox/index.js
@@ -2,13 +2,24 @@ import { useState } from "react";
 import "./style.css";
 
 function InputBox() {
+  const [text, setText] = useState();
+
+  function handleInputTextBoxChange(e) {
+    return setText(e.target.value);
+  }
+
   return (
     <>
       <div className="container">
         <label>
-          write something:
-          <input />
+          write something:{" "}
+          <input
+            value={text}
+            onChange={handleInputTextBoxChange}
+            // onChange={(e) => setText(e.target.value)}
+          />
         </label>
+        <p className="textContainer">{text}</p>
       </div>
     </>
   );
